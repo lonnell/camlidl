bf027a58590aeb2f38feebc02c831658b38aebdb
Lonnell Ahiyya <lonnell@mekanisys.com>
2016-10-19 10:23:05 -0400

    Getting a sane build.

--- common.mk
+++ common.mk
@@ -1,8 +1,8 @@
 OCAMLMAKEFILE 	= $(TOPDIR)/OCamlMakefile 
-SOURCES		= $(patsubst %, %.mly, $(MLYFILES))
-SOURCES		+= $(patsubst %, %.mll, $(MLLFILES))
-SOURCES 	+= $(patsubst %, %.mli, $(MLIFILES))
-SOURCES 	+= $(patsubst %, %.ml, $(MLFILES))
+# SOURCES		= $(patsubst %, %.mly, $(MLYFILES))
+# SOURCES		+= $(patsubst %, %.mll, $(MLLFILES))
+# SOURCES 	+= $(patsubst %, %.mli, $(MLIFILES))
+# SOURCES 	+= $(patsubst %, %.ml, $(MLFILES))
 INCDIRS		:= $(patsubst %, ../%, $(modeps))
 LIBDIRS		:= $(INCDIRS) 
 CMIFILES	= $(wildcard *.cmi)
