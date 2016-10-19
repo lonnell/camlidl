161394fc5833f608efc0f9ee9c3f545361fb5dde
Lonnell Ahiyya <lonnell@mekanisys.com>
2016-10-19 10:42:51 -0400

    More of the same.

--- common.mk
+++ common.mk
@@ -1,8 +1,6 @@
 OCAMLMAKEFILE 	= $(TOPDIR)/OCamlMakefile 
-# SOURCES		= $(patsubst %, %.mly, $(MLYFILES))
-# SOURCES		+= $(patsubst %, %.mll, $(MLLFILES))
-# SOURCES 	+= $(patsubst %, %.mli, $(MLIFILES))
-# SOURCES 	+= $(patsubst %, %.ml, $(MLFILES))
+SOURCES 	= $(patsubst %, %.mli, $(MLIFILES))
+SOURCES 	+= $(patsubst %, %.ml, $(MLFILES))
 INCDIRS		:= $(patsubst %, ../%, $(modeps))
 LIBDIRS		:= $(INCDIRS) 
 CMIFILES	= $(wildcard *.cmi)
